17f370f4b68cfba84b902fd17c45fc665fdad199
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-19 11:57:05 +0100

    exercise2

---
+++ exercise2/c2-ec2-instance.tf
@@ -0,0 +1,12 @@
+# Create EC2 Instance
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = "ami-0742b4e673072066f"
+  instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
